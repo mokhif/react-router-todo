c6613ee24133f4a1eb17d14a79081b4397102a26
mokhif <dunosixen@gmail.com>
2026-03-01 01:54:55 +0100

    setting-dotenv-mongoose

--- my-server/server.js
+++ my-server/server.js
@@ -1,9 +1,14 @@
+import dotenv from "dotenv";
 import express from "express";
+import mongoose from "mongoose";
+dotenv.config();
 const app = express();
 const port = 5000;
-
+console.log(process.env.MONGODB_URI)
 app.get("/", (req, res) => {
   res.status(200).send("hello world");
 });
 
+await mongoose.connect(process.env.MONGODB_URI);
+console.log("connected to mongodb");
 app.listen(port, () => console.log(`server is running on port:${port}`));

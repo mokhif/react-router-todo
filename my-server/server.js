25a1556ab0b2de36c89acdf85f4b403e0aef8988
mokhif <dunosixen@gmail.com>
2026-03-04 01:31:01 +0100

    register route added

--- my-server/server.js
+++ my-server/server.js
@@ -1,12 +1,24 @@
 import dotenv from "dotenv";
 import express from "express";
-import mongoose from "mongoose";
+import mongoose, { Schema } from "mongoose";
+import cookieParser from "cookie-parser";
+import User from "./models/users.js";
 dotenv.config();
+import jwt from "jsonwebtoken";
+app.use(cookieParser());
+app.use(express.json());
 const app = express();
 const port = 5000;
 console.log(process.env.MONGODB_URI);
 app.get("/", (req, res) => {
   res.status(200).send("hello world");
+});
+//REgiostration Route
+Router.post("/register", async (req, res) => {
+  const { name, email, password } = req.body;
+  const user = await User.crate({ name, email, password });
+  generateToken(res, user._id);
+  res.status(200).json({ msg: "Registered successfully" });
 });
 
 async function main() {
